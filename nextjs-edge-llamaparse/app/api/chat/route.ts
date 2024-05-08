93920bf1d9e5879b92a8bcfc70bf35dd49e14a45
gabelegorburu <168595815+gabelegorburu@users.noreply.github.com>
2024-05-07 20:54:35 -0700

    Update route.ts

--- nextjs-edge-llamaparse/app/api/chat/route.ts
+++ nextjs-edge-llamaparse/app/api/chat/route.ts
@@ -45,8 +45,8 @@ export async function POST(request: NextRequest) {
     }
 
     const llm = new OpenAI({
-      model: (process.env.MODEL as any) ?? "gpt-3.5-turbo",
-      maxTokens: 512,
+      model: (process.env.MODEL as any) ?? "gpt-4-turbo",
+      maxTokens: 3000,
     });
 
     const chatEngine = await createChatEngine(llm);
